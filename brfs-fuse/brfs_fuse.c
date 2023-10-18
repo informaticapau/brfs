355536a44abcd5ca1044c69b5e4ab6077b3be038
arf20 <aruizfernandez05@gmail.com>
2023-10-18 22:32:40 +0200

    Playing with FUSE

--- brfs-fuse/brfs_fuse.c
+++ brfs-fuse/brfs_fuse.c
@@ -25,6 +25,7 @@
 #include <errno.h>
 
 #include <unistd.h>
+#include <sys/types.h>
 
 
 #define FUSE_USE_VERSION    26
@@ -39,25 +40,57 @@
 
 
 int
-brfs_fuse_getattr(const char *path, struct stat *) {
-    debug_log(1, "brfs_fuse_getattr(path=%s,)\n", path);
+brfs_fuse_getattr(const char *path, struct stat *st) {
+    debug_log(1, "getattr(\"%s\")\n", path);
+
+    st->st_uid = getuid(); // The owner of the file/directory is the user who mounted the filesystem
+	st->st_gid = getgid(); // The group of the file/directory is the same as the group of the user who mounted the filesystem
+	st->st_atime = time( NULL ); // The last "a"ccess of the file/directory is right now
+	st->st_mtime = time( NULL );
+
+    if (strcmp(path, "/") == 0) {
+		st->st_mode = S_IFDIR | 0755;
+		st->st_nlink = 2; // Why "two" hardlinks instead of "one"? The answer is here: http://unix.stackexchange.com/a/101536
+	}
+	else {
+		st->st_mode = S_IFREG | 0644;
+		st->st_nlink = 1;
+		st->st_size = 1024;
+	}
+
+    return 0;
+}
+
+int brfs_fuse_readdir(const char *path, void *buffer, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi) {
+    debug_log(1, "readdir(\"path\")\n", path);
+
+    filler(buffer, ".", NULL, 0); // Current Directory
+	filler(buffer, "..", NULL, 0); // Parent Directory
+
+    if ( strcmp( path, "/" ) == 0 ) // If the user is trying to show the files/directories of the root directory show the following
+	{
+		filler( buffer, "file54", NULL, 0 );
+		filler( buffer, "file349", NULL, 0 );
+	}
+
     return 0;
 }
 
 int
 brfs_fuse_open(const char *path, struct fuse_file_info *) {
-    debug_log(1, "brfs_fuse_open\n");
+    debug_log(1, "open(\"%s\")\n", path);
     return 0;
 }
 
 int
 brfs_fuse_read(const char *path, char *, size_t, off_t, struct fuse_file_info *) {
-    debug_log(1, "brfs_fuse_read\n");
+    debug_log(1, "read(\"%s\")\n", path);
     return 0;
 }
 
 struct fuse_operations brfs_operations = {
     .getattr = brfs_fuse_getattr,
+    .readdir = brfs_fuse_readdir,
     .open = brfs_fuse_open,
     .read = brfs_fuse_read
 };
